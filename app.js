61b20880aa09608da5c6ac57d1ccc4f7aa1543c3
Dana Sugu <dana.sugu@gmail.com>
2021-07-14 18:30:14 +0300

    Update app.js

--- app.js
+++ app.js
@@ -3,3 +3,18 @@ const taskList = document.querySelector('.collection');
 const clearBtn = document.querySelector('.clear-tasks');
 const filter = document.querySelector('#filter');
 const taskInput = document.querySelector('#task');
+
+//load all event listeners
+
+loadEventListeners();
+
+function loadEventListeners(){
+  //add task event
+  form.addEventListener('submit', addTask);
+
+  //add task
+
+  function addTask() {
+    
+  }
+}
