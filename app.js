cfb395d0341d424585cf94e9c8e782b488d13ea9
Dana Sugu <dana.sugu@gmail.com>
2021-07-19 19:10:47 +0300

    Update app.js

--- app.js
+++ app.js
@@ -66,3 +66,16 @@ function clearTasks() {
     taskList.removeChild(taskList.firstChild);
   }
 }
+// Filter Tasks
+function filterTasks(e) {
+  const text = e.target.value.toLowerCase();
+
+  document.querySelectorAll('.collection-item').forEach(function (task) {
+    const item = task.firstChild.textContent;
+    if (item.toLowerCase().indexOf(text) != -1) {
+      task.style.display = 'block';
+    } else {
+      task.style.display = 'none';
+    }
+  });
+}
