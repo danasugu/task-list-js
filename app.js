c54f4d0f35e87cf6eb3be2b81d344d744da8ae2d
Dana Sugu <dana.sugu@gmail.com>
2021-07-21 16:00:49 +0300

    Update app.js

--- app.js
+++ app.js
@@ -62,6 +62,7 @@ function storeTaskInLocalStorage(task) {
     tasks = JSON.parse(localStorage.getItem('tasks'));
   }
   tasks.push(task);
+  localStorage.setItem('tasks', JSON.stringify(tasks));
 }
 
 // Remove Task
